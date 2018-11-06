53f12b66a8084705dbd0d12d1629367871877bae
jacky <jacky@dafacloud.com>
2018-11-06 11:22:17 +0800

    Initial Commit

---
+++ GeoLocation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoLocation
+//
+//  Created by ＴＩＮＧＹＵ on 2018/11/6.
+//  Copyright © 2018年 ＴＩＮＧＹＵ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
